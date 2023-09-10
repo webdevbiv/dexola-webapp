764496568b91518be93984ebd51efb3247d787da
webdevbiv <80968651+webdevbiv@users.noreply.github.com>
2023-09-10 12:08:59 +0300

    UPD | button

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -2,12 +2,18 @@ import { HeaderButton } from "../HeaderButton/HeaderButton";
 import { Logo } from "../Logo/Logo";
 import s from "./Header.module.scss";
 
+const handleWalletConnect = () => {
+  console.log("handleWalletConnect");
+};
 export const Header = () => {
   return (
     <header className={s.header}>
       <div className={s.container}>
         <Logo />
-        <HeaderButton text={"connect wallet"} />
+        <HeaderButton
+          text={"connect wallet"}
+          onClick={handleWalletConnect}
+        />
       </div>
     </header>
   );
--- src/components/HeaderButton/HeaderButton.jsx
+++ src/components/HeaderButton/HeaderButton.jsx
@@ -1,10 +1,18 @@
 import PropTypes from "prop-types";
 import s from "./HeaderButton.module.scss";
 
-export const HeaderButton = ({ text }) => {
-  return <button className={s.button}>{text}</button>;
+export const HeaderButton = ({ text, onClick }) => {
+  return (
+    <button
+      className={s.button}
+      onClick={onClick}
+    >
+      {text}
+    </button>
+  );
 };
 
 HeaderButton.propTypes = {
+  onClick: PropTypes.func.isRequired,
   text: PropTypes.string.isRequired,
 };
