2a825787d88ab0d775c456175be32fe823f02e07
webdevbiv <80968651+webdevbiv@users.noreply.github.com>
2023-09-14 22:11:05 +0300

    ADD | scroll to footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,3 +1,21 @@
+import s from "./Footer.module.scss";
+
 export const Footer = () => {
-  return <footer>Footer</footer>;
+  return (
+    <footer>
+      <div className={s.container}>
+        <div className={s.wrapper}>
+          <a
+            href='https://dexola.com'
+            rel='noopener noreferrer'
+            target='_blank'
+            className={s.text}
+          >
+            Designed by Dexola - 2023
+          </a>
+          <p className={s.text}>© All rights reserved</p>
+        </div>
+      </div>
+    </footer>
+  );
 };
