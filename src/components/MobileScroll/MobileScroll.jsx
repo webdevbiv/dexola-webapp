4ef606d9fd9f052d662470cbf17362351ce42e8b
webdevbiv <80968651+webdevbiv@users.noreply.github.com>
2023-09-15 16:07:10 +0300

    FIX | Scroll

--- src/components/MobileScroll/MobileScroll.jsx
+++ src/components/MobileScroll/MobileScroll.jsx
@@ -1,7 +1,7 @@
 import PropTypes from "prop-types";
 import s from "./MobileScroll.module.scss";
 export const MobileScroll = ({ children }) => {
-  return <div className={s.scroll}>{children}</div>;
+  return <div className='scroll'>{children}</div>;
 };
 
 MobileScroll.propTypes = {
