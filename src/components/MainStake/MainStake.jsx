a1139abfa7bb3f0c8594a6147b6c4a672fb81228
webdevbiv <80968651+webdevbiv@users.noreply.github.com>
2023-09-23 02:01:42 +0300

    FIX | no imports

--- src/components/MainStake/MainStake.jsx
+++ src/components/MainStake/MainStake.jsx
@@ -1,145 +1,79 @@
-import { useState } from "react";
-import { useAccount, useWaitForTransaction } from "wagmi";
-import { parseEther } from "viem";
-import {
-  useUserBalanceOfStarRunner,
-  useRewardRateForUser,
-  useContractWrite,
-  useContractRead,
-} from "../../Hooks";
-
-import { CONTRACT } from "../../constants/constants";
-import { MainTitle } from "../MainTitle/MainTitle";
-import { MainContainer } from "../MainContainer/MainContainer";
-import { MainForm } from "../MainForm/MainForm";
-import { sanitizeInputValue } from "../../utils/utils";
-
-export const MainStake = () => {
-  const [inputValue, setInputValue] = useState("");
-  const { address: userWalletAddress } = useAccount();
-  const balanceToDisplay = Number(
-    useUserBalanceOfStarRunner(userWalletAddress).formatted
-  ).toFixed(1);
-
-  const amountToApprove = parseEther(inputValue.toString());
-
-  const { data: userStakedBalanceOfStarRunner } = useContractRead({
-    functionName: "balanceOf",
-    args: [userWalletAddress],
-    watch: true,
-  });
-
-  const rewardRate = useRewardRateForUser(
-    inputValue,
-    userStakedBalanceOfStarRunner
-  );
-
-  const {
-    data: approveData,
-    isLoading: approveIsLoading,
-    isSuccess: approveIsSuccess,
-    write: approveWrite,
-  } = useContractWrite({
-    functionName: "approve",
-    token: true,
-  });
-
-  const {
-    data: waitForApprove,
-    isError: waitForApproveIsError,
-    isLoading: waitForApproveIsLoading,
-  } = useWaitForTransaction({
-    hash: approveData?.hash,
-    onSettled(data, error) {
-      console.log("Settled waitForApprove", { data, error });
-      stakeWrite({ args: [amountToApprove] });
-    },
-  });
-
-  const {
-    data: stakeData,
-    isLoading: stakeIsLoading,
-    isSuccess: stakeIsSuccess,
-    write: stakeWrite,
-  } = useContractWrite({ functionName: "stake" });
-
-  const {
-    data: waitForStake,
-    isError: waitForStakeIsError,
-    isLoading: waitForStakeIsLoading,
-  } = useWaitForTransaction({
-    hash: stakeData?.hash,
-    onSettled(data, error) {
-      console.log("Settled waitForStake", { data, error });
-      setInputValue("");
-    },
-  });
-
-  const handleChange = (e) => {
-    const sanitizedValue = sanitizeInputValue(e.target.value);
-    setInputValue(sanitizedValue);
-  };
-
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    approveWrite({
-      args: [CONTRACT, amountToApprove],
-    });
-  };
-
-  const isAnyLoading =
-    approveIsLoading ||
-    stakeIsLoading ||
-    waitForApproveIsLoading ||
-    waitForStakeIsLoading;
-
-  return (
-    <MainContainer>
-      <MainTitle
-        pageName='Stake'
-        rewardRate={rewardRate}
-      />
-      <MainForm
-        handleSubmit={handleSubmit}
-        handleChange={handleChange}
-        inputValue={inputValue}
-        isAnyLoading={isAnyLoading}
-        balanceToDisplay={balanceToDisplay}
-        buttonText={"Stake"}
-      />
-    </MainContainer>
-  );
-};
-
 // import { useState } from "react";
-// import { useAccount } from "wagmi";
+// import { useAccount, useWaitForTransaction } from "wagmi";
 // import { parseEther } from "viem";
-
-// // Hooks
 // import {
 //   useUserBalanceOfStarRunner,
 //   useRewardRateForUser,
-//   useApproveAndStake,
+//   useContractWrite,
+//   useContractRead,
 // } from "../../Hooks";
-// import { sanitizeInputValue } from "../../utils/utils";
 
-// // Components
+// import { CONTRACT } from "../../constants/constants";
 // import { MainTitle } from "../MainTitle/MainTitle";
 // import { MainContainer } from "../MainContainer/MainContainer";
 // import { MainForm } from "../MainForm/MainForm";
-
-// // Constants
-// import { CONTRACT } from "../../constants/constants";
+// import { sanitizeInputValue } from "../../utils/utils";
 
 // export const MainStake = () => {
 //   const [inputValue, setInputValue] = useState("");
 //   const { address: userWalletAddress } = useAccount();
-//   const userBalanceOfStarRunner = useUserBalanceOfStarRunner(userWalletAddress);
-//   const rewardRate = useRewardRateForUser(inputValue);
-//   const amountApprove = parseEther(inputValue.toString());
+//   const balanceToDisplay = Number(
+//     useUserBalanceOfStarRunner(userWalletAddress).formatted
+//   ).toFixed(1);
 
-//   const { approveWrite, approveIsLoading, stakeIsLoading } =
-//     useApproveAndStake(amountApprove);
+//   const amountToApprove = parseEther(inputValue.toString());
+
+//   const { data: userStakedBalanceOfStarRunner } = useContractRead({
+//     functionName: "balanceOf",
+//     args: [userWalletAddress],
+//     watch: true,
+//   });
+
+//   const rewardRate = useRewardRateForUser(
+//     inputValue,
+//     userStakedBalanceOfStarRunner
+//   );
+
+//   const {
+//     data: approveData,
+//     isLoading: approveIsLoading,
+//     isSuccess: approveIsSuccess,
+//     write: approveWrite,
+//   } = useContractWrite({
+//     functionName: "approve",
+//     token: true,
+//   });
+
+//   const {
+//     data: waitForApprove,
+//     isError: waitForApproveIsError,
+//     isLoading: waitForApproveIsLoading,
+//   } = useWaitForTransaction({
+//     hash: approveData?.hash,
+//     onSettled(data, error) {
+//       console.log("Settled waitForApprove", { data, error });
+//       stakeWrite({ args: [amountToApprove] });
+//     },
+//   });
+
+//   const {
+//     data: stakeData,
+//     isLoading: stakeIsLoading,
+//     isSuccess: stakeIsSuccess,
+//     write: stakeWrite,
+//   } = useContractWrite({ functionName: "stake" });
+
+//   const {
+//     data: waitForStake,
+//     isError: waitForStakeIsError,
+//     isLoading: waitForStakeIsLoading,
+//   } = useWaitForTransaction({
+//     hash: stakeData?.hash,
+//     onSettled(data, error) {
+//       console.log("Settled waitForStake", { data, error });
+//       setInputValue("");
+//     },
+//   });
 
 //   const handleChange = (e) => {
 //     const sanitizedValue = sanitizeInputValue(e.target.value);
@@ -149,12 +83,15 @@ export const MainStake = () => {
 //   const handleSubmit = (e) => {
 //     e.preventDefault();
 //     approveWrite({
-//       args: [CONTRACT, amountApprove],
+//       args: [CONTRACT, amountToApprove],
 //     });
-//     setInputValue("");
 //   };
 
-//   const isAnyLoading = approveIsLoading || stakeIsLoading;
+//   const isAnyLoading =
+//     approveIsLoading ||
+//     stakeIsLoading ||
+//     waitForApproveIsLoading ||
+//     waitForStakeIsLoading;
 
 //   return (
 //     <MainContainer>
@@ -167,8 +104,71 @@ export const MainStake = () => {
 //         handleChange={handleChange}
 //         inputValue={inputValue}
 //         isAnyLoading={isAnyLoading}
-//         balanceOfStarRunner={userBalanceOfStarRunner}
+//         balanceToDisplay={balanceToDisplay}
+//         buttonText={"Stake"}
 //       />
 //     </MainContainer>
 //   );
 // };
+
+// // import { useState } from "react";
+// // import { useAccount } from "wagmi";
+// // import { parseEther } from "viem";
+
+// // // Hooks
+// // import {
+// //   useUserBalanceOfStarRunner,
+// //   useRewardRateForUser,
+// //   useApproveAndStake,
+// // } from "../../Hooks";
+// // import { sanitizeInputValue } from "../../utils/utils";
+
+// // // Components
+// // import { MainTitle } from "../MainTitle/MainTitle";
+// // import { MainContainer } from "../MainContainer/MainContainer";
+// // import { MainForm } from "../MainForm/MainForm";
+
+// // // Constants
+// // import { CONTRACT } from "../../constants/constants";
+
+// // export const MainStake = () => {
+// //   const [inputValue, setInputValue] = useState("");
+// //   const { address: userWalletAddress } = useAccount();
+// //   const userBalanceOfStarRunner = useUserBalanceOfStarRunner(userWalletAddress);
+// //   const rewardRate = useRewardRateForUser(inputValue);
+// //   const amountApprove = parseEther(inputValue.toString());
+
+// //   const { approveWrite, approveIsLoading, stakeIsLoading } =
+// //     useApproveAndStake(amountApprove);
+
+// //   const handleChange = (e) => {
+// //     const sanitizedValue = sanitizeInputValue(e.target.value);
+// //     setInputValue(sanitizedValue);
+// //   };
+
+// //   const handleSubmit = (e) => {
+// //     e.preventDefault();
+// //     approveWrite({
+// //       args: [CONTRACT, amountApprove],
+// //     });
+// //     setInputValue("");
+// //   };
+
+// //   const isAnyLoading = approveIsLoading || stakeIsLoading;
+
+// //   return (
+// //     <MainContainer>
+// //       <MainTitle
+// //         pageName='Stake'
+// //         rewardRate={rewardRate}
+// //       />
+// //       <MainForm
+// //         handleSubmit={handleSubmit}
+// //         handleChange={handleChange}
+// //         inputValue={inputValue}
+// //         isAnyLoading={isAnyLoading}
+// //         balanceOfStarRunner={userBalanceOfStarRunner}
+// //       />
+// //     </MainContainer>
+// //   );
+// // };
