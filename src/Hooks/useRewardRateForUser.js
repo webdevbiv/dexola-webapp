f28d5ac446343fcdbf4ce3e52c53756a9e0511cb
webdevbiv <80968651+webdevbiv@users.noreply.github.com>
2023-09-22 12:09:08 +0300

    UPD | useRewardRateForUser refactor

--- src/Hooks/useRewardRateForUser.js
+++ src/Hooks/useRewardRateForUser.js
@@ -1,55 +1,45 @@
 import { useContractRead } from "./useContractRead";
-import { formatEther, parseEther } from "viem";
+import { formatEther } from "viem";
 
 export const useRewardRateForUser = (
   inputValue,
   userStakedBalanceOfStarRunner
 ) => {
+  const SECONDS_IN_A_WEEK = 7 * 24 * 60 * 60;
+
+  // Formatting input values
   const inputValueFormatted = Number(inputValue);
-  console.log(`inputValueFormatted`, inputValueFormatted);
-  const userStakedBalanceOfStarRunnerFormatted = Number(
+  const userStakedBalanceFormatted = Number(
     formatEther(userStakedBalanceOfStarRunner)
   );
 
-  console.log(
-    `userStakedBalanceOfStarRunnerFormatted`,
-    userStakedBalanceOfStarRunnerFormatted
-  );
-  console.log(`inputValue`, inputValue);
-  const SECONDS_IN_A_WEEK = 7 * 24 * 60 * 60;
+  // Fetching contract data
   const { data: periodFinish } = useContractRead({
     functionName: "periodFinish",
   });
-  const periodFinishFormatted = Number(periodFinish);
-  console.log(`periodFinishFormatted`, periodFinishFormatted);
   const { data: rewardRate } = useContractRead({ functionName: "rewardRate" });
-  const rewardRateFormatted = Number(formatEther(rewardRate));
-  console.log(`rewardRateFormatted`, rewardRateFormatted);
-
   const { data: totalSupply } = useContractRead({
     functionName: "totalSupply",
   });
+
+  // Formatting contract data
+  const periodFinishFormatted = Number(periodFinish);
+  const rewardRateFormatted = Number(formatEther(rewardRate));
   const totalSupplyFormatted = Number(formatEther(totalSupply));
-  console.log(`totalSupplyFormatted`, totalSupplyFormatted);
 
-  // Calculate total available rewards
+  // Calculations
   const currentTimestamp = Math.floor(Date.now() / 1000);
-  console.log(`currentTimestamp`, currentTimestamp);
-  const remaining = periodFinishFormatted - currentTimestamp;
-  console.log(`remaining`, remaining);
-  const weeksRemaining = Math.ceil(remaining / SECONDS_IN_A_WEEK);
-  const totalAvailableRewards = remaining * rewardRateFormatted;
+  const remainingSeconds = periodFinishFormatted - currentTimestamp;
+  const weeksRemaining = Math.ceil(remainingSeconds / SECONDS_IN_A_WEEK);
+  const totalAvailableRewards = remainingSeconds * rewardRateFormatted;
 
-  // Calculate reward rate using the formula
   const rewardRateForUser =
-    (userStakedBalanceOfStarRunnerFormatted * totalAvailableRewards) /
+    (userStakedBalanceFormatted * totalAvailableRewards) /
       totalSupplyFormatted +
     inputValueFormatted;
+  const rewardRateForUserPerWeek = Math.floor(
+    rewardRateForUser / weeksRemaining
+  ).toFixed(0);
 
-  console.log(`rewardRateForUser`, rewardRateForUser);
-  const rewardRateForUserPerWeek = rewardRateForUser / weeksRemaining;
-
-  const result = rewardRateForUserPerWeek.toFixed(1);
-
-  return result;
+  return rewardRateForUserPerWeek;
 };
