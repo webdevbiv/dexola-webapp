76cc6684ba0015bb3a2a8f0a5d211695ee3927f3
webdevbiv <80968651+webdevbiv@users.noreply.github.com>
2023-09-23 00:10:30 +0300

    FIX | MainConnectWallet enabled

--- src/pages/ConnectWallet.jsx
+++ src/pages/ConnectWallet.jsx
@@ -3,8 +3,7 @@ import { MainConnectWallet } from "../components/MainConnectWallet/MainConnectWa
 export const ConnectWallet = () => {
   return (
     <>
-      {/* <MainConnectWallet /> */}
-      <div>Connect Wallet</div>
+      <MainConnectWallet />
     </>
   );
 };
