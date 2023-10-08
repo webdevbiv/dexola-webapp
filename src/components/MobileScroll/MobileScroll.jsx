4aa0de3b86ec29243f2528518b8cdd62abfc8b2e
webdevbiv <80968651+webdevbiv@users.noreply.github.com>
2023-10-08 21:39:06 +0300

    DEL | MobileScroll

--- src/components/MobileScroll/MobileScroll.jsx
+++
@@ -1,9 +0,0 @@
-import PropTypes from "prop-types";
-import s from "./MobileScroll.module.scss";
-export const MobileScroll = ({ children }) => {
-  return <div className='scroll'>{children}</div>;
-};
-
-MobileScroll.propTypes = {
-  children: PropTypes.node.isRequired,
-};
---
+++ src/components/TooltipModal/TooltipModal.jsx
@@ -0,0 +1,3 @@
+export const TooltipModal = () => {
+  return <div>TooltipModal</div>;
+};
