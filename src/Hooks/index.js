d61222b696488a13fdffdbd9fdeb3b9fc6605cea
webdevbiv <80968651+webdevbiv@users.noreply.github.com>
2023-10-21 19:58:21 +0300

    DEL | useContractWrite

--- src/Hooks/index.js
+++ src/Hooks/index.js
@@ -1,6 +1,5 @@
 export { useApproveAndStake } from "./useApproveAndStake";
 export { useContractRead } from "./useContractRead";
-export { useContractWrite } from "./useContractWrite";
 export { useRewardRateForUser } from "./useRewardRateForUser";
 export { useWindowWidth } from "./useWindowWidth";
 export { useWithdrawUserData } from "./useWithdrawUserData";
